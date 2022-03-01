c62823e40b44af03276f61d3a6b4d9c586204207
Pfefan <stefanpfeiffer04@gmail.com>
2022-03-01 10:23:29 +0100

    fixed most pylint errors

--- authenticator.py
+++ authenticator.py
@@ -1,40 +1,28 @@
-"""module imports"""
-from discord.ext import commands
-
 import databasecmd
+from discord.ext import commands
+from threading import Thread
 
-
-class DCcmd(commands.Cog):
-    """class to define commands"""
-
+class authenticate(commands.Cog):
     def __init__(self, client):
-        """init variables"""
-
         self.client = client
         self.cmd = databasecmd.CMD()
-
+        
     @commands.command()
-    async def rand(self, ctx):
-        """command to get random server with players online"""
-
-        await self.cmd.getrandserver(ctx)
-
+    async def rand (self, ctx):
+       await self.cmd.getrandserver(ctx)
+  
     @commands.command()
-    async def online(self, ctx, message="top"):
-        """command to get all servers with players online"""
+    async def online (self, ctx, message="top"):
         await self.cmd.searchservers(ctx, message)
 
     @commands.command()
-    async def info(self, ctx, message = ""):
-        """command to get detailed info about a specific server"""
-        await self.cmd.info(ctx, message)
+    async def details (self, ctx, message=None):
+        await self.cmd.details(ctx, message)
 
     async def on_reaction(self, reaction, user):
-        """func to execute code on reaction"""
         msg = reaction.message
-        if str(msg.author) == "MC-servers#6007":
+        if(str(msg.author) == "MC-servers#6007"):
             await self.cmd.checkreaction(reaction, user)
 
 def setup(client):
-    """setup"""
-    client.add_cog(DCcmd(client))
+    client.add_cog(authenticate(client))
